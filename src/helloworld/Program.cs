fb2691ef1ab63317f32305c0859d9820ccd9211f
barnabeepickle <64658127+barnabeepickle@users.noreply.github.com>
2025-10-28 11:32:50 -0400

    added code

---
+++ src/helloworld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloworld.src.helloworld
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
